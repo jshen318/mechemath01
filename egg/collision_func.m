343a9179970c336f6dd5d4f1e91bb47f85999f29
ssperou <shauna.sperou@gmail.com>
2026-09-19 18:46:43 -0400

    finished egg smash animate, no vid yet

--- egg/collision_func.m
+++ egg/collision_func.m
@@ -17,18 +17,37 @@ function [t_ground,t_wall] = collision_func(traj_func, y_ground, x_wall)
     t_ground = NaN;
     t_wall = NaN;
 
-
-    guess0_list = linspace(0,7,25);
-    guess1_list = linspace(0,7,25);
+    iter = 25;
+    guess0_list = linspace(0,0.5,iter);
+    guess1_list = linspace(0,0.5,iter);
     [x0_list, x1_list] = meshgrid(guess0_list, guess1_list);
 
-    for t = 1:25^2
+    
+    
+    for t = 1:iter^2
         x_guess = x0_list(t);
         x1_guess = x1_list(t);
-        traj_wrapper_x = @(t) traj_wrapper1(t,traj_func,egg_params,x_wall,1);
-        % traj_wrapper_y = @(t) traj_wrapper1(t,traj_func,egg_params,y_ground,2);
 
-        [x_root, xit_flag] = secant_solver(traj_wrapper_x, x_guess, x1_guess)
-        % [y_root, exyt_flag] = bisection_solver(traj_wrapper_y(t), 1, 7)
+        traj_wrapper_x = @(t) traj_wrapper1(t,traj_func,egg_params,x_wall,1);
+        traj_wrapper_y = @(t) traj_wrapper1(t,traj_func,egg_params,y_ground,2);
+
+        if isnan(t_wall)
+            [x_root, xit_flag] = secant_solver(traj_wrapper_x, x_guess, x1_guess);
+            x_root
+            if xit_flag == 1 && x_root > 0
+                t_wall = x_root;
+            end
+        end
+
+        if isnan(t_ground)
+            [y_root, exyt_flag] = secant_solver(traj_wrapper_y, x_guess, x1_guess);
+            y_root
+            if exyt_flag == 1 && y_root > 0
+                t_ground = y_root;
+            end
+        end
+
     end
+    % t_ground = y_root
+    % t_wall = x_root
 end
--- egg/eggnimation.m
+++ egg/eggnimation.m
@@ -14,8 +14,15 @@ function [t_ground,t_wall] = eggnimation(traj_fun, y_ground, x_wall)
     egg_params = struct();
     egg_params.a = 3; egg_params.b = 2; egg_params.c = .15;
     
-    t_ground = NaN;
-    t_wall = NaN;
+    % t_ground = NaN;
+    % t_wall = NaN;
+    [t_ground, t_wall] = collision_func(traj_fun, y_ground, x_wall);
+
+    if t_ground > t_wall
+        time = t_wall
+    elseif t_wall > t_ground
+        time = t_ground
+    end
 
     % %specify the position and orientation of the egg
     % x0 = 5; y0 = 5; theta = pi/6;
@@ -25,11 +32,14 @@ function [t_ground,t_wall] = eggnimation(traj_fun, y_ground, x_wall)
     
     wallLine = xline(x_wall);
     set(egg_plot, 'xdata', wallLine);
+    
+    time_iter = linspace(0,time,100)
 
-    for t = 0:0.05:1.29
+    for t = time_iter
+       
         [x, y, theta] = traj_fun(t);
-        
-        
+
+
         axis([0,x_wall,0,30])
         
         %plot the origin of the egg frame
@@ -43,20 +53,6 @@ function [t_ground,t_wall] = eggnimation(traj_fun, y_ground, x_wall)
         [x_range,y_range] = compute_bounding_box(egg_params, x, y, theta);
 
       
-        % delta_y = y_range(1) - y_ground;
-        % delta_x = x_range(2) - x_wall;
-        % tol = 1e-2;
-        % 
-        % if delta_y < tol && t_ground == 0
-        %     t_ground = t;
-        %     t_wall = NaN;
-        % end
-        % 
-        % if delta_xdelta_x < tol && t_wall == 0
-        %     t_wall = t;
-        %     t_ground = NaN;
-        % 
-        % end
 
         xbox = [x_range(1), x_range(1), x_range(2), x_range(2), x_range(1)];
         ybox = [y_range(1), y_range(2), y_range(2), y_range(1), y_range(1)];
@@ -69,5 +65,6 @@ function [t_ground,t_wall] = eggnimation(traj_fun, y_ground, x_wall)
         
         %update the actual plotting window
         drawnow;
+        t
     end
 end
